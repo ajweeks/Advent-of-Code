765e450d8434d356169a1ec3b5fd6b3e8a8e9b9e
ajweeks <ajweeks@shaw.ca>
2018-12-06 20:00:24 +0000

    Finally add full solution for day 06

--- 2018/day06.py
+++ 2018/day06.py
@@ -26,15 +26,7 @@ for coord in coords:
 
 print(max_x, max_y)
 
-possibleIdces = [x for x in coords]
-
-for coord in coords:
-    if coord[0] == 0 or coord[0] == max_x or \
-       coord[1] == 0 or coord[1] == max_y:
-        possibleIdces.remove(coord)
-
 print(coords)
-print(possibleIdces)
 
 grid = [999] * (max_x * max_y)
 for yy in range(max_y):
@@ -65,7 +57,10 @@ for yy in range(max_y):
         if c:
             print("$", end='')
         else:
-            print(chr(ord('A') + grid[yy * max_x + xx]), end='')
+            if grid[yy * max_x + xx] == -1:
+                print('`', end='')
+            else:
+                print(chr(ord('A') + grid[yy * max_x + xx]), end='')
     print()
 
 largestFiniteSize = 0
@@ -76,25 +71,53 @@ for i in range(len(coords)):
 
 for yy in range(max_y):
     for xx in range(max_x):
-        coverage[grid[yy * max_x + xx]][0] += 1
+        if grid[yy * max_x + xx] != -1:
+            coverage[grid[yy * max_x + xx]][0] += 1
 
 coverage = sorted(coverage, reverse=True)
 
-# print(coverage, "largestFiniteSize:", largestFiniteSize, "largestFiniteIdx:", largestFiniteIdx)
+print(coverage, "largestFiniteSize:", largestFiniteSize, "largestFiniteIdx:", largestFiniteIdx)
 
 print(coverage[0][0], chr(ord('A') + coverage[0][1]), coverage)
 
 for i in range(len(coverage)):
-    onEdge = False
-    for yy in range(max_y):
+    isInfinite = False
+    # Top and bottom rows
+    for yy in [0, max_y - 1]:
         for xx in range(max_x):
             if grid[yy * max_x + xx] == coverage[i][1]:
                 if xx <= 0 or xx > max_x - 1 or \
-                   yy <= 0 or yy > max_y - 1:
-                    onEdge = True
+                        yy <= 0 or yy > max_y - 1:
+                    isInfinite = True
                     break
-        if onEdge:
+        if isInfinite:
             break
-    if not onEdge:
-        print(chr(ord('A') + coverage[i][1]), coverage[i])
+    # Right and left columns
+    for yy in range(max_y):
+        for xx in [0, max_x - 1]:
+            if grid[yy * max_x + xx] == coverage[i][1]:
+                if xx <= 0 or xx > max_x - 1 or \
+                   yy <= 0 or yy > max_y - 1:
+                    isInfinite = True
+                    break
+        if isInfinite:
+            break
+    if not isInfinite:
+        print(f"not infinite: {chr(ord('A') + coverage[i][1])}, coverage: {coverage[i][0]}, index: {coverage[i][1]}")
 
+numWithinRange = 0
+threshold = 10_000
+hthresh = int(threshold / 100)
+for yy in range(-hthresh, max_y + hthresh):
+    for xx in range(-hthresh, max_x + hthresh):
+        dist = 0
+        for c in coords:
+            dist += manhattan(c[0], c[1], xx, yy)
+            if dist > threshold:
+                break
+        if dist < threshold:
+            numWithinRange += 1
+    if yy % 100 == 0:
+        print(yy, -hthresh, max_y + hthresh, numWithinRange)
+
+print(f"spots within {threshold} of all coordinates: {numWithinRange}")
