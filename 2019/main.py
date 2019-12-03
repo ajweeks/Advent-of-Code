2e4de4d694eddb3c0232f79494dba586a2ecdeee
AJ Weeks <ajweeks@shaw.ca>
2019-12-03 21:24:46 +0000

    Improve day printing

--- 2019/main.py
+++ 2019/main.py
@@ -1,11 +1,28 @@
 
 import day1, day2, day3
 
-# day1.part1()
-# day1.part2()
+funcs = [
+    (day1.part1, day1.part2),
+    (day2.part1, day2.part2),
+    (day3.part1, day3.part2),
+]
 
-# day2.part1()
-# day2.part2()
 
-day3.part1()
-day3.part2()
+def print_latest():
+    print_pair(len(funcs) - 1, funcs[-1])
+
+
+def print_all():
+    for idx, func_pair in enumerate(funcs):
+        print_pair(idx, func_pair)
+
+
+def print_pair(idx, pair):
+    print("Day ", idx + 1, ":\n\t", sep='', end='')
+    pair[0]()
+    print("\t", end='')
+    pair[1]()
+
+
+# print_all()
+print_latest()
