012ee6ff08d95cd241efbc5b39cf4adb00dc05c6
AJ Weeks <ajweeks@shaw.ca>
2019-12-04 07:15:27 +0000

    Add solution for day 04

---
+++ 2019/day4.py
@@ -0,0 +1,56 @@
+def get_inputs():
+    return [int(y) for y in open("day4.in").readlines()[0].split('-')]
+
+
+def meets_criteria(num, p2):
+    digits = [ord(x) for x in str(num)]
+    for idx, d in enumerate(digits):
+        for i in range(0, idx):
+            if d < digits[i]:
+                return False
+    has_double = False
+    i = 0
+    while i < len(digits) - 1:
+        if digits[i] == digits[i + 1]:
+            if p2 and has_double:
+                return False
+            has_double = True
+            if p2:
+                if i + 2 < len(digits):
+                    if digits[i] == digits[i + 2]:
+                        has_double = False  # Triple or more
+                        count = 2 if (i + 1) == (len(digits) - 1) else len(digits) - i
+                        for j in range(i, len(digits)):
+                            if digits[j] != digits[i]:
+                                count = j - i
+                                break
+                        i += count
+                        continue
+                    else:
+                        break
+                else:
+                    break  # End of the num with 2
+            if not p2:
+                break
+        i += 1
+    if not has_double:
+        return False
+    # print(num)
+    return True
+
+
+def get_count(p2):
+    inputs = get_inputs()
+    count = 0
+    for i in range(inputs[0], inputs[1]+1):
+        if meets_criteria(i, p2):
+            count += 1
+    print(count)
+
+
+def part1():
+    get_count(False)
+
+
+def part2():
+    get_count(True)
--- 2019/main.py
+++ 2019/main.py
@@ -1,10 +1,11 @@
 
-import day1, day2, day3
+import day1, day2, day3, day4
 
 funcs = [
     (day1.part1, day1.part2),
     (day2.part1, day2.part2),
     (day3.part1, day3.part2),
+    (day4.part1, day4.part2),
 ]
 
 
